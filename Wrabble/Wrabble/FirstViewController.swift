1d7d873dbd0c7aeee753451075a7684bcfdaa17d
Bart de Bruin <bart.fr.debruin@gmail.com>
2016-01-07 16:54:28 +0100

    First commit of Wrabble, setting up project as Application with tabbar

---
+++ Wrabble/Wrabble/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Wrabble
+//
+//  Created by Bart de Bruin on 07-01-16.
+//  Copyright © 2016 Wrabble. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
